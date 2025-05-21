2a0aef3fffcfd59a5729ccbe9e9898fb9e4ab746
CarEsteban <car23016@uvg.edu.gt>
2025-05-21 12:54:14 -0600

    story book for button

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,7 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/stories/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-essentials",
---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Button from '../components/Button.jsx'
+
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    value: { control: 'text' },
+    type: {
+      control: { type: 'select' },
+      options: ['number','operator','special','function'],
+    },
+    onClick: { action: 'clicked' },
+  },
+}
+
+const Template = (args) => <Button {...args} />
+
+export const NumberKey = Template.bind({})
+NumberKey.args = { value: '7', type: 'number' }
+
+export const OperatorKey = Template.bind({})
+OperatorKey.args = { value: '+', type: 'operator' }
+
+export const FunctionKey = Template.bind({})
+FunctionKey.args = { value: 'AC', type: 'function' }
+
+export const SpecialKey = Template.bind({})
+SpecialKey.args = { value: '%', type: 'special' }
