0329df1d9fbafd761d8bc97ae4e8cb839cb2a41b
CarEsteban <car23016@uvg.edu.gt>
2025-05-23 18:42:27 -0600

    lint

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,7 +6,7 @@ import Button from './Button.jsx'
 export default function Calculator () {
   const { keys, expression, displayResult, onKey } = useCalculator()
   return (
-    <div className="w-screen min-h-[100dvh] max-h-[100dvh] flex flex-col bg-gray-900 p-4  rounded-none shadow-2xl pb-[env(safe-area-inset-bottom)]  sm:rounded-2xl sm:w-120 sm:mx-auto  sm:min-h-[70vh]">
+    <div className='w-screen min-h-[100dvh] max-h-[100dvh] flex flex-col bg-gray-900 p-4  rounded-none shadow-2xl pb-[env(safe-area-inset-bottom)]  sm:rounded-2xl sm:w-120 sm:mx-auto  sm:min-h-[70vh]'>
       <Display expression={expression} result={displayResult} />
       <div className='grid grid-cols-4 grid-rows-5 gap-2 mt-2 auto-rows-fr flex-1 pb-5'>
         {keys.map(({ value, type, wide }, i) => (
